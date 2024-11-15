9bc2f8e2518d46c542ba2e7a71ab1effe59a70c1
Ygor Pessoa <ygoryuri@deliveryhero.com>
2024-11-15 22:33:55 +0100

    Initial Commit

---
+++ LottieColorizerMacOSApp/LottieColorizerMacOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieColorizerMacOSAppApp.swift
+//  LottieColorizerMacOSApp
+//
+//  Created by Ygor Yuri De Pinho Pessoa on 15.11.24.
+//
+
+import SwiftUI
+
+@main
+struct LottieColorizerMacOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
